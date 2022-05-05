04fd3c95d57deae16acaa00e71a053a7ce6ab1e1
Hari-dhisigma <102206326+Hari-dhisigma@users.noreply.github.com>
2022-05-05 05:42:53 -0700

    Update Profile.js

--- Profile.js
+++ Profile.js
@@ -2,7 +2,8 @@ import React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import { Link } from "react-router-dom";
-import './P.css';
+import './Profile.css';
+
 
 
 function Profile() {
