facd98ef8e45279cd5bd5f6ce865a95094ef9eb4
Hari-dhisigma <102206326+Hari-dhisigma@users.noreply.github.com>
2022-05-05 21:11:23 -0700

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
 import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Venue from "./components/Venue";
-import Profile from "./components/Me";
-import Qbook from "./components/Qbook";
+import Profile from "./components/Profile";
 import Start from "./components/Start";
-import Offers from "./components/offers";
-import './App.css';
+import Offers from "./components/Offers";
+import "./App.css";
 
 function App() {
   return (
@@ -13,13 +12,10 @@ function App() {
       <Router>
         <div>
           <Routes>
-            
-            {/* <Route path="/" element={<Venue />} /> */}
-            <Route path="/" element={<Profile />} />
-            {/* <Route path="/" element={<Qbook />} /> */}
+            <Route path="/" element={<Venue />} />
+            {/* <Route path="/" element={<Profile />} /> */}
             {/* <Route path="/" element={<Start />} /> */}
             {/* <Route path="/" element={<Offers />} />  */}
-
           </Routes>
         </div>
       </Router>
